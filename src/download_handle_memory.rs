d009d5172dadf598ada9843c1eef3af26c60b6d2
林纯杰 <linchunjie@xmfunny.com>
2023-10-30 15:26:54 +0800

    fix cursor

--- src/download_handle_memory.rs
+++ src/download_handle_memory.rs
@@ -7,7 +7,7 @@ use crate::download_handle::{DownloadHandleTrait};
 
 pub struct DownloadHandleMemory {
     config: Arc<Mutex<DownloadConfiguration>>,
-    bytes: Option<Vec<u8>>,
+    cursor: Option<Cursor<Vec<u8>>>,
     downloaded_size: u64,
 }
 
@@ -15,14 +15,14 @@ impl DownloadHandleMemory {
     pub fn new(config: Arc<Mutex<DownloadConfiguration>>) -> Self {
         Self {
             config,
-            bytes: None,
+            cursor: None,
             downloaded_size: 0,
         }
     }
 
     pub fn text(&mut self) -> String {
-        if let Some(bytes) = &mut self.bytes {
-            if let Ok(text) = String::from_utf8(bytes.clone()) {
+        if let Some(bytes) = self.bytes() {
+            if let Ok(text) = String::from_utf8(bytes) {
                 return text;
             }
         }
@@ -30,8 +30,21 @@ impl DownloadHandleMemory {
     }
 
     pub fn bytes(&mut self) -> Option<Vec<u8>> {
-        if let Some(bytes) = &mut self.bytes {
-            return Some(bytes.clone());
+        if let Some(cursor) = &mut self.cursor {
+            let len = cursor.get_ref().len();
+            let mut buffer: Vec<u8> = Vec::with_capacity(len);
+            unsafe {
+                buffer.set_len(len);
+            }
+            cursor.set_position(0);
+            match cursor.read_exact(&mut buffer) {
+                Ok(_) => {
+                    return Some(buffer);
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            }
         }
         None
     }
@@ -42,14 +55,19 @@ impl DownloadHandleTrait for DownloadHandleMemory {
     async fn setup(&mut self) -> Result<(), Box<dyn Error + Send>> {
         let size = self.config.lock().await.total_length;
         let buffer: Vec<u8> = Vec::with_capacity(size as usize);
-        self.bytes = Some(buffer);
+        self.cursor = Some(Cursor::new(buffer));
         Ok(())
     }
 
     async fn received_bytes_async(&mut self, poition: u64, buffer: &Vec<u8>) -> Result<(), Box<dyn Error + Send>> {
         self.downloaded_size += buffer.len() as u64;
-        if let Some(bytes) = &mut self.bytes {
-            bytes.extend_from_slice(&buffer);
+        if let Some(cursor) = &mut self.cursor {
+            if let Err(e) = cursor.seek(SeekFrom::Start(poition)) {
+                return Err(Box::new(e));
+            }
+            if let Err(e) = cursor.write_all(buffer) {
+                return Err(Box::new(e));
+            }
         }
         Ok(())
     }
