f5f97720805eac056cf239ea27b666fdcf3e58f2
林纯杰 <linchunjie@xmfunny.com>
2023-10-30 19:22:20 +0800

    update public mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod download_handle;
+pub mod download_handle;
 mod download_task;
 mod stream;
 mod downloader;
@@ -6,8 +6,8 @@ mod remote_file;
 mod chunk;
 mod chunk_metadata;
 mod chunk_hub;
-mod download_configuration;
-mod download_service;
+pub mod download_configuration;
+pub mod download_service;
 mod c;
 mod download_handle_file;
 mod download_handle_memory;
